a78024618cf2510282c4c66d97d397a82d9e62ee
Joshua Cutts <josh.cutts1@gmail.com>
2025-12-01 16:08:16 -0800

    complete day1 part1

--- day_1/day1_part1.py
+++ day_1/day1_part1.py
@@ -1,4 +1,30 @@
-"""
+def parse_input(file):
+    with open(file, "r") as f:
+        lines = f.read().splitlines()
+    
+    return lines
 
 
-"""
+def count_zero_position_during_rotations(instructions):
+    zero_count = 0
+    dial = 50
+
+    for instruction in instructions:
+        direction = instruction[0]
+        num = int(instruction[1:]) % 100
+
+        num = num if direction == "R" else (num * -1)
+        dial += num
+        
+        if dial >= 100:
+            dial = dial % 100
+        elif dial < 0:
+            dial = 100 + dial
+        
+        zero_count += (dial == 0)
+
+    return zero_count
+
+instructions = parse_input("part1_input.txt")
+print(count_zero_position_during_rotations(instructions))
+
