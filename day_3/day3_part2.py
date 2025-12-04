aea63235eb351ccef10722d2022a869507953448
Joshua Cutts <josh.cutts1@gmail.com>
2025-12-03 23:18:43 -0800

    solve day 3 part 2

---
+++ day_3/day3_part2.py
@@ -0,0 +1,55 @@
+def parse_input(filename):
+    with open(filename, "r") as f:
+        grid = [list(row) for row in f.read().splitlines()]
+
+    return grid
+
+def count_adjacent_rolls(grid, row_i, col_i):
+    adjacent_rolls = 0
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+
+            adjacent_row = row_i + i
+            adjacent_col = col_i + j
+            
+            if adjacent_row >= 0 and adjacent_col >= 0 and adjacent_row < len(grid) and adjacent_col < len(grid[0]):
+                if grid[adjacent_row][adjacent_col] == '@':
+                    
+                    adjacent_rolls += 1
+    
+    return adjacent_rolls
+
+
+# print(count_adjacent_rolls(grid, 0, 3)) # 3
+
+def count_removed_rolls(grid):
+    removed_rolls = 0
+    roll_removed = True
+
+    while roll_removed:
+        roll_removed = False
+
+        for row_i in range(len(grid)):
+            row = grid[row_i]
+            for col_i in range(len(row)):
+                if grid[row_i][col_i] == '@':
+                    adjacent_rolls = count_adjacent_rolls(grid, row_i, col_i)
+
+                    if adjacent_rolls < 4:
+                        roll_removed = True
+                        grid[row_i][col_i] = '.'
+                        removed_rolls += 1
+                    
+
+    return removed_rolls
+
+
+# grid = parse_input('example.txt')
+grid = parse_input('input.txt')
+print(count_removed_rolls(grid))
+
+
+
