f5b82ebb0efffe162b37f28b01e0a4b9a75b6065
Joshua Cutts <josh.cutts1@gmail.com>
2025-12-07 21:12:36 -0800

    solve day 2 part 2

--- day_6/day6_part2.py
+++ day_6/day6_part2.py
@@ -1,69 +1,54 @@
 import math
+import re
 
 def parse_input(filename):
     with open(filename, 'r') as f:
-        raw_input = [line.split() for line in f.read().splitlines()]
-
-    for i, line in enumerate(raw_input[:-1]):
-        raw_input[i] = [int(n) for n in line]
+        raw_input = [line for line in f.read().splitlines()]
 
     return raw_input
 
 def vertical_align_problems(raw_input):
     problems = []
+    operators = raw_input[-1].split()
+    problem = []
+    skip = False
+    for col in range(len(raw_input[0])):
+        if skip:
+            print(problem)
+            problems.append(problem)
+            problem = []
+            skip = False
+            next
+        num = ''
+        for row in range(len(raw_input[0:-1])):
+            num += raw_input[row][col]
+        if all(x == ' ' for x in num.split()):
+            skip = True
+        else:
+            problem.append(num)
+            num = ''
 
-    for row in range(len(raw_input[0])):
-        problem = []
-        for col in range(len(raw_input)):
-            problem.append(raw_input[col][row])
+    problems.append(problem)
 
-        problems.append(problem)
-    
-    return problems
-
-def transform_problem(original, operator):
-    max_length = (max([len(str(n)) for n in original]))
-    
-    if operator == '+':
-        justified = [str(n).ljust(max_length, ' ') for n in original]
-    else:
-        justified = [str(n).rjust(max_length, ' ') for n in original]
-    
-    matrix_digits = [list(n) for n in justified]
-
-    transformed = []
-
-    for col in range(len(matrix_digits[0]) - 1, -1, -1):
-        new_num = ''
-        for row in range(0, len(matrix_digits)):
-            new_num += matrix_digits[row][col]
-        
-        transformed.append(new_num)
-
-    return [int(x) for x in transformed]
+    combined_data = list(zip(problems, operators))
+    return [numbers + [operator] for numbers, operator in combined_data]
 
 def calculate_total(problems):
     total = 0
 
     for problem in problems:
         operator = problem.pop()
-        transformed_problem = transform_problem(problem, operator)
-        print(transformed_problem)
+        int_problem = [int(num) for num in problem]
+
         if operator == '+':
-            total += sum(transformed_problem)
+            total += sum(int_problem)
         else:
-            total += math.prod(transformed_problem)
+            total += math.prod(int_problem)
 
     return total
 
-
-raw_input = parse_input('example.txt')
-# raw_input = parse_input('puzzle.txt')
-# print(raw_input)
+# raw_input = parse_input('example.txt')
+raw_input = parse_input('puzzle.txt')
 problems = vertical_align_problems(raw_input)
 # print(problems)
 print(calculate_total(problems))
-# print(transform_problem([64, 23, 314], '+'))
-# print(transform_problem([51, 387, 215], '*'))
-
-
